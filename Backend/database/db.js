31ffdba52df5809bb0b06757a20b9e3ef32227d3
Ashish-Sonkar <ashishsonkar3452@gmail.com>
2026-09-21 18:08:01 +0530

    Initial commit

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -3,21 +3,9 @@ const mongoose=require("mongoose")
 
 async function dbConnection(){
     await mongoose.connect(process.env.DB_CONNECT_KEY)
-    // await mongoose.connect("mongodb://ashishsonkar3452_db_user:Ashish%40123@ac-zr8omab-shard-00-00.bzhusry.mongodb.net:27017,ac-zr8omab-shard-00-01.bzhusry.mongodb.net:27017,ac-zr8omab-shard-00-02.bzhusry.mongodb.net:27017/?ssl=true&replicaSet=atlas-5mgfo3-shard-0&authSource=admin&appName=Coding/File-Sharing")
 }
 
-// dbConnection()
-// .then(()=>console.log("db connected"))
-// .catch((err)=>console.log("Error"+err.message))
+
 
 module.exports=dbConnection 
 
-// const dns = require('dns');
-
-// dns.resolveSrv('_mongodb._tcp.coddingadda.ce8o9vp.mongodb.net', (err, addresses) => {
-//   if (err) {
-//     console.error('DNS SRV resolution failed on this system:', err);
-//   } else {
-//     console.log('DNS SRV resolved successfully:', addresses);
-//   }
-// });
